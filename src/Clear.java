9f5afaff76331a65f4cf20094ca3b85459cdc1d1
bugiman113 <63304289+bugiman113@users.noreply.github.com>
2020-04-28 13:33:15 +0300

    Delete Clear.java

--- src/Clear.java
+++
@@ -1,6 +0,0 @@
-public class Clear {
-    public String clear(){
-        Collection.collection.clear();
-        return ( "Коллекция очищена"+"\nколичество элементов: " + Collection.getCollection().size());
-    }
-}
