53efa6a33797a86dd27863a6a2ea080197c385c5
bugiman113 <63304289+bugiman113@users.noreply.github.com>
2020-05-25 16:26:25 +0300

    Delete Info.java

--- src/src.Server/Info.java
+++
@@ -1,5 +0,0 @@
-public class Info {
-    public String inf(){
-        return ("Тип коллекции " + Collection.map.getClass() + "!Дата инициализации: " + Collection.getTime() + "!количество элементов: " + Collection.map.entrySet().stream().mapToInt((p)->1).sum());//Collection.getCollection().size()
-    }
-}
